3b3078bddc0bd6381cde7beeadea3aa74e2e0ddd
司才浅 <1617181071@qq.com>
2025-04-05 00:19:00 +0800

    美工阶段结束
    1.美工素材处理完毕，导入项目，需要程序进行处理和优化。
    2.美术素材待处理优化。

--- assets/script/food.gd
+++ assets/script/food.gd
@@ -22,7 +22,7 @@ func _physics_process(_delta: float) -> void:
 func _on_input_event(_viewport ,event, _shape_idx):
 #	检测鼠标点击事件
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed: 
+		if event.pressed:
 			
 			# 只有鼠标在物体上按下时才允许进行拖拽，get_rect() 作用是获取 Sprite节点 的边界
 			if $Sprite2D.get_rect().has_point(to_local(event.position)):
